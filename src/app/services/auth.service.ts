23b4230f938bd6c278225b0fe5464e7123af45db
zajno <zjn700-01@yahoo.com>
2019-02-19 05:42:33 +0000

    added Google login for web, but control does not return to the app. Will press on with native login and the db service to see if either resolves this.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,6 +7,12 @@ import { Observable, of } from 'rxjs';
 import { switchMap, take, map } from 'rxjs/operators';
 import { DbService } from './db.service';
 
+import { Platform } from '@ionic/angular';
+
+import { LoadingController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,11 +22,17 @@ export class AuthService {
   constructor(
     private afAuth: AngularFireAuth,
     private db: DbService,
-    private router: Router
+    private router: Router,
+    private platform: Platform,
+    private loadingController: LoadingController,
+    private storage: Storage
   ) { 
     this.user$ = this.afAuth.authState.pipe(
       // switchMap(user => (user ? db.doc$(`users/${user.uid}`) : of(null)))
     );
+
+    this.handleRedirect();
+
   }
 
   async anonymousLogin() {
@@ -30,7 +42,7 @@ export class AuthService {
 
   private updateUserData({ uid, email, displayName, photoURL, isAnonymous }) {
   //   // Sets user data to firestore on login
-
+    console.log('user', uid)
     const path = `users/${uid}`;
 
     const data = {
@@ -40,7 +52,8 @@ export class AuthService {
       photoURL,
       isAnonymous,
     };
-    
+    console.log("update user data", uid, displayName, photoURL, isAnonymous)
+    return
   //   return this.db.updateAt(path, data);
   }
 
@@ -49,5 +62,57 @@ export class AuthService {
     return this.router.navigate(['/']);
   }
 
+
+//// GOOGLE AUTH
+
+  setRedirect(val) {
+    this.storage.set('authRedirect', val);
+  }
+
+  async isRedirect() {
+    return await this.storage.get('authRedirect');
+  }
+
+  async googleLogin() {
+    try {
+      let user;
+      console.log('googleLogin')
+      if (this.platform.is('cordova')) {
+        console.log('cordova')
+        user = await this.nativeGoogleLogin();
+      } else {
+        console.log('web')
+        await this.setRedirect(true);
+        const provider = new auth.GoogleAuthProvider();
+        user = await this.afAuth.auth.signInWithRedirect(provider);
+        console.log('user', user)
+      }
+
+      return await this.updateUserData(user);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // Handle login with redirect for web Google auth
+  private async handleRedirect() {
+    if ((await this.isRedirect()) !== true) {
+      return null;
+    }
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    const result = await this.afAuth.auth.getRedirectResult();
+
+    if (result.user) {
+      await this.updateUserData(result.user);
+    }
+
+    await loading.dismiss();
+
+    await this.setRedirect(false);
+
+    return result;
+  }
 
 }
