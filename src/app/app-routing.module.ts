f65f29f29dac353ed26cb046efb3456daee2e159
zajno <zjn700-01@yahoo.com>
2019-02-24 02:15:06 +0000

    updated app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,8 +12,8 @@ const routes: Routes = [
   { path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate: [TutorialGuard] },
   // { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'todo', loadChildren: './todo/todo.module#TodoPageModule', canActivate: [AuthGuard] },
-  { path: 'tutorial', loadChildren: './tutorial/tutorial.module#TutorialPageModule' },
-  { path: '**', redirectTo: 'todo', pathMatch: 'full'}
+  { path: 'tutorial', loadChildren: './tutorial/tutorial.module#TutorialPageModule' }
+  // { path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
